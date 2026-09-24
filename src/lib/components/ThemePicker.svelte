ff16a0adc68c8599c7017f0c73e7cb84b2a56a07
fomel4ik <fomel4ik.yt@gmail.com>
2026-09-25 01:58:59 +0700

    added themeSwitcher

--- src/lib/components/ThemePicker.svelte
+++ src/lib/components/ThemePicker.svelte
@@ -1,20 +1,46 @@
 <script lang="ts">
-	
+	import { browser } from '$app/environment';
+
+	type Theme = 'system' | 'light' | 'dark';
+
+	let theme = $state<Theme>('system'); // хранит в себе тему, по умолчанию system
+
+	$effect(() => { // проверка есть ли тема в localStorage
+		if (!browser) return;
+
+		const savedTheme = localStorage.getItem('theme');
+
+		if (savedTheme === 'light' || savedTheme === 'dark' || savedTheme === 'system') {
+			theme = savedTheme;
+		}
+	});
+
+	$effect(() => {
+		if (!browser) return;
+
+		if (theme === 'system') {
+			document.documentElement.removeAttribute('data-theme');
+		} else {
+			document.documentElement.dataset.theme = theme;
+		}
+
+		localStorage.setItem('theme', theme);
+	});
 </script>
 
 <fieldset>
-    <div>
-        <input type="radio" id="theme-picker" name="theme" value="system" />
-        <label for="theme-picker">System</label>
-    </div>
-    <div>
-        <input type="radio" id="theme-picker" name="theme" value="light" />
-        <label for="theme-picker">Light</label>
-    </div>
-    <div>
-        <input type="radio" id="theme-picker" name="theme" value="dark" />
-        <label for="theme-picker">Dark</label>
-    </div>
+    <label class="theme-button">
+        <input type="radio" bind:group={theme} value="system" />
+        <span>System</span>
+    </label>
+    <label class="theme-button">
+        <input type="radio" bind:group={theme} value="light" />
+        <span>Light</span>
+    </label>
+    <label class="theme-button">
+        <input type="radio" bind:group={theme} value="dark" />
+        <span>Dark</span>
+    </label>
 </fieldset>
 
 <style>
